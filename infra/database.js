83f1e9472e3a206eb56d36920ab0ffd3022644e2
Pedro Mello <pedroadmn@gmail.com>
2024-07-05 17:21:22 -0500

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -21,7 +21,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
   await client.connect();
@@ -32,3 +32,13 @@ export default {
   query,
   getNewClient,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
